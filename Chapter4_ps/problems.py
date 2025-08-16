0c1e47a0dd4231c16369a111a1ea915ce268c016
Arnav Saxena <saxena.arnav0405@gmail.com>
2025-08-16 16:13:28 +0530

    Add files via upload

---
+++ Chapter4_ps/problems.py
@@ -0,0 +1,34 @@
+#Q1 Write a program to store 4 Marks in a list entered by the user
+#Marks = []
+#f1 = int(input("Enter Marks here:  ")
+#Marks.append(f1) #since we created an empty list we will have to keep appending all the list constituents and here the list of Marks is tuple itself
+
+#f2 = int(input("Enter Marks here:  ")
+#Marks.append(f2) 
+
+#f3 = int(input("Enter Marks here:  ")
+#Marks.append(f3) 
+
+#f4 = int(input("Enter Marks here:  ")
+#Marks.append(f4) 
+
+#print(Marks)
+
+#Q2 write a program to display the marks of 4 students and display them in a sorted matter
+Marks = []
+f1 = int(input("Enter Marks here:  "))
+Marks.append(f1) #since we created an empty list we will have to keep appending all the list constituents and here the list of Marks is tuple itself
+
+f2 = int(input("Enter Marks here:  "))
+Marks.append(f2) 
+
+f3 = int(input("Enter Marks here:  "))
+Marks.append(f3) 
+
+f4 = int(input("Enter Marks here:  "))
+Marks.append(f4) 
+
+Marks.sort() #since the input is string based therefore it will sort alphabetically the numbers
+print(Marks)
+# islie har int ko int(input) banana zaruri for marks data type number
+
