93c51c19b2ff4c9578414c12fc24e6964e9aae92
Arnav Saxena <saxena.arnav0405@gmail.com>
2026-01-03 15:41:52 +0530

    Add files via upload

---
+++ Chapter_7/02_while_loops.py
@@ -0,0 +1,16 @@
+i = 1
+
+while(i<51):
+    print(i)
+    i +=1 # += means if while is executed when condition is fullfilled to increment i by 1 that is now i=2
+    # executes till condition is satisfied
+    # while loop has to be exited islie condition aise dalte
+
+'''
+output:
+1
+2
+3
+4
+5
+'''
