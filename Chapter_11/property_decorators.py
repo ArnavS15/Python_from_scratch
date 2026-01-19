eee3a048e18cfc77804a07381cd63a26b70097e3
Arnav Saxena <saxena.arnav0405@gmail.com>
2026-01-20 00:17:06 +0530

    Add files via upload

--- Chapter_11/property_decorators.py
+++ Chapter_11/property_decorators.py
@@ -1,21 +1,21 @@
-class Employee:
+class Employee: # bhot line of code humne sirf class employee me bharke encapsulate kardia ie bhot units ko ek class me pack kardia
     a = 1
 
     @classmethod
     def show(cls):
-        print(f"the class attribute of a a is {cls.a}")
+        print(f"the class attribute of a is {cls.a}")
 
-    @property
+    @property # property decorator me name is a function jo chalega but make it seem like its a property ki .name and return karke print karke hume firt name last name set karke miljaye thus abstraction ki user ko dikhega nhi the effort put in
     def name(self):
-        return self.ename
+        return f"{self.fname} {self.lname}" # encapsulation me pata bhi ni chalega ki ek result ke lie kitna code backend pe hai
 
     @name.setter
     def name(self, value): # string is split such that ["Harry", "khan"] is a list of strings after the split at " " in which ie Harry is the 0th element and khan is the first string set as first name and last name respectfully]
         self.fname = value.split(" ")[0] # f for first name 
-        self.lname = value.split("")[1] # l for last name
+        self.lname = value.split(" ")[1] # l for last name
     
 e = Employee()
 e.a = 45
 e.name = "Harry Khan"
-print(e.name)
+print(e.fname, e.lname)
 e.show()
