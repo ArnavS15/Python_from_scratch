16e57a82fd6a2dce0ba008be820c8e4aede5fea9
Arnav Saxena <saxena.arnav0405@gmail.com>
2026-01-30 23:27:56 +0530

    Add files via upload

---
+++ 3Python/math_library.py
@@ -0,0 +1,20 @@
+import math
+x = 10.8
+# functions in math library are listed below:
+print(math.ceil(x)) # rounded up value
+print(math.floor(x)) # rounded down value
+print(math.trunc(x)) # truncate removes the decimal part and gives value
+
+
+x = 3
+print(math.exp(x))
+print(math.log10(x))
+
+y =90 
+print(math.sin(y))
+print(math.cos(y))
+print(math.tan(y))
+
+print(math.e)
+print(math.pi)
+print(10,math.factorial(10))
---
+++ 3Python/random_library.py
@@ -0,0 +1,14 @@
+# i have covered this from code with harry
+import random
+
+print(random.random())
+print(random.seed(40))
+print(random.choice([1,2,3,4,5]))
+print(random.randint(1,11))
+print(random.sample([1,2,3,4,5],2))
+print(random.uniform(1,3))
+
+# date and time we have covered
+# collections and counter and defaultdict dekhlena
+# ordered dicts and strings as well
+# import me dekhlena acii_lowercase ascii_uppercase so on
